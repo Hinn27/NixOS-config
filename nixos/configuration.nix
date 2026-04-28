3ed76b5e2efb052eafb1a91e63505dac2e668bbd
Huynh Kim Duc <duc107243@donga.edu.vn>
2026-04-28 13:10:30 +0700

    Sửa user system trong NixOS

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -53,21 +53,13 @@
   networking.hostName = "hinne";
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
-  users.users = {
-    # FIXME: Replace with your username
-    hinne = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      initialPassword = "correcthorsebatterystaple";
-      isNormalUser = true;
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = [ "wheel" "networkmanager" "audio" "video" ]; # thêm group hữu ích
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      ];
-    };
-  };
+  users.users.hinne = {
+  isNormalUser = true;
+  description = "Hin";
+  extraGroups = [ "wheel" "networkmanager" "docker" ]; # thêm group nếu cần
+  shell = pkgs.zsh; # hoặc bash nếu bạn thích
+  # optional: initialPassword = "yourpass"; # chỉ dùng lần đầu
+};
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
